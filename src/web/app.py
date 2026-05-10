c6fe4718db04fcb4ca320f5a6c529c9df2874f26
Alex Krusz <harfatum@gmail.com>
2026-05-09 17:03:05 -0700

    min app size

--- src/web/app.py
+++ src/web/app.py
@@ -311,7 +311,7 @@ def _run_webview(app, socketio, host: str, port: int, window_mode: str = "rememb
         "frameless": frameless,
         "easy_drag": False,
         "vibrancy": vibrancy,
-        "min_size": (1024, 600),
+        "min_size": (390, 700),
     }
 
     if window_mode == "fullscreen":
